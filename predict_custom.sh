c8e449a656a76fab5be98021b2aa10b4c61b78e3
Nicola Piga <nicolapiga@gmail.com>
2022-09-05 20:14:16 +0000

    Update predict_custom.sh

--- predict_custom.sh
+++ predict_custom.sh
@@ -1,1 +1,5 @@
-python /home/user/iros20-6d-pose-tracking/predict_custom.py --object_name 006_mustard_bottle --sequence_path /home/user/iros20-6d-pose-tracking/datasets/sequence/
+for item in mustard_translation_x_gt_velocity_1m_s; do
+    # python /home/user/iros20-6d-pose-tracking/predict_custom.py --sequence_path /home/user/iros20-6d-pose-tracking/datasets/$item
+    cd /home/user/iros20-6d-pose-tracking/datasets/
+    zip -r /home/user/iros20-6d-pose-tracking/results/mustard_translation_x_gt_velocity_1m_s_se3_results.zip $item/output/*
+done
