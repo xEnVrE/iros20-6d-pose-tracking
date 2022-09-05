c8a92397ac3f789fc89209c58d0c2e0a9dc6ba4d
Nicola Piga <nicolapiga@gmail.com>
2022-09-05 20:59:28 +0000

    predict_custom.sh also render a video of the output

--- predict_custom.sh
+++ predict_custom.sh
@@ -3,7 +3,9 @@ for item in mustard_translation_x_gt_velocity_1m_s; do
        rm /home/user/iros20-6d-pose-tracking/datasets/$item/output/*
     fi
     python /home/user/iros20-6d-pose-tracking/predict_custom.py --sequence_path /home/user/iros20-6d-pose-tracking/datasets/$item
+    cd /home/user/iros20-6d-pose-tracking/datasets/$item/output
+    ffmpeg -framerate 60.0 -pattern_type glob -i '*.png' -c:v libx264 -pix_fmt yuv420p "./${item}_rendering.mp4"
     cd /home/user/iros20-6d-pose-tracking/datasets/
-    zip -r /home/user/iros20-6d-pose-tracking/results/mustard_translation_x_gt_velocity_1m_s_se3_results.zip $item/output/*
+    zip -r /home/user/iros20-6d-pose-tracking/results/mustard_translation_x_gt_velocity_1m_s_se3_results.zip $item/output/$item*
     cd -
 done
